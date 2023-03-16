dc5af61c6ce6c5179b5da92a9279c3ad74f13f6a
rajy <kiddo.rajy@gmail.com>
2023-03-16 14:33:57 -0700

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,6 @@ int main(void)
 	printf("size of a long int: %i byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %i byte(s)\n", sizeof(long long int));
 	printf("size of a float: %i byte(s)\n", sizeof(float));
-	fprintf(stderr, "Anything\n");
+	fprintf(stderr, "[Anything]\n");
 	return (0);
 }
