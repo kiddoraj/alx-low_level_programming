545dcfa156c85c2d3b7e6d8bd6ee2f23db86aad4
rajy <kiddo.rajy@gmail.com>
2023-03-21 09:20:55 -0700

    islower

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -16,6 +16,7 @@ void print_alphabet_x10(void)
 			_putchar(c);
 			c++;
 		}
+
 		_putchar('\n');
 		i++;
 	}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercsse character
+ *
+ * Return: 1 if c is lowercase0 otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
