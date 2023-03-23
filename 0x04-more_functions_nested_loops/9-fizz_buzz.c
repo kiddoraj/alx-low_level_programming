43050ef1cdae068aa2f30027c86dfa53e27eccfd
rajy <kiddo.rajy@gmail.com>
2023-03-23 12:24:28 -0700

    fix buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if( i % 3 == 0 && i % 5 != 0)
+		if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf("Fizz");
 		}
@@ -29,8 +29,10 @@ int main(void)
 		}
 		else
 		{
-			printf("%d", i);
+			printf(" %d", i);
 		}
 	}
-	printf('\n');
+	printf("\n");
+
+	return (0);
 }
