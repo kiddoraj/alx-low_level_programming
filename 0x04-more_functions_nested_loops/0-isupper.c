75827d43b88a0572a7f078900b33bde45cd63283
rajy <kiddo.rajy@gmail.com>
2023-03-23 10:17:48 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_isupper - checks for the uppercase character
+ *
+ * @c: character
+ *
+ * Return: i if c is uppercase
+ * 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef FILE_NAME
+#define FILE_NAME
+
+int _isupper(int c);
+
+
+#endif
