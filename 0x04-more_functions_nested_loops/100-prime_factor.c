2abf57324408cf0a1329a141c2a43ea495cb860f
rajy <kiddo.rajy@gmail.com>
2023-03-24 08:12:06 -0700

    fix

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,40 +1,24 @@
-#include "main.h"
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main - finds and prints the largest prime factor of 
- * the number 612852475143
- * followed by a new line
+ * main -  calculate largest prime of 612852475143
  *
- * Return: Always 0
+ * Return: Success Always
  */
+
+
 int main(void)
 {
-	long int n;
-	long int max;
-	long int i;
+	long int x = 612852475143;
+	long int py;
 
-	n = 612852475143;
-	max = -1;
-
-	while (n % 2 == 0)
+	for (py = 2; py < x; py++)
 	{
-		max = 2;
-		n /= 2;
-	}
-
-	for (i = 3; i <= sqrt(n); i = i + 2)
-	{
-		while (n % i == 0)
+		if (x % py == 0)
 		{
-			max = i;
-			n = n / i;
+			x = x / py;
 		}
 	}
-
-	if (n > 2)
-		max = n;
-
-	printf("%ld\n", max);
-
+	printf("%ld\n", py);
 	return (0);
 }
--- 0x04-more_functions_nested_loops/101-Print_number.c
+++ 0x04-more_functions_nested_loops/101-Print_number.c
@@ -1,18 +1,30 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
 
 /**
- * print_number - prints an integer
- * @n: integer to be printed
+ * print_number - like a hello world
+ *
+ * @n: params an integer
+ *
+ * No return
  */
+
+
 void print_number(int n)
 {
-	unsigned int n1;
 
-	if (n < 0)
+	unsigned int n1 = 0;
+
+	if  (n < 0)
 	{
 		n1 = -n;
 		_putchar('-');
-	} else
+	}
+
+	else
 	{
 		n1 = n;
 	}
