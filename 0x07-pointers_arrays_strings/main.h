ba6893ba304954f393eda7c5f39c0b34b8e1cd07
rajy <kiddo.rajy@gmail.com>
2023-04-03 12:29:42 -0700

    strstr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,10 +3,11 @@
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
+ichar *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
 
 
