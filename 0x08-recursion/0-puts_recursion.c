41f2c45454875a442e57bf4716290190109e3d41
rajy <kiddo.rajy@gmail.com>
2023-04-05 11:24:44 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *void_recursion - prints a string
+ *
+ *@s:string
+ */
+void _puts_recursion(char *s)
+{
+	_puts_recursion();
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,10 @@
+#ifndef FULL_NAME
+#define FULL_NAME
+
+int _putchar(char *c);
+void _puts_recursion(char *s);
+
+
+
+
+#endif
