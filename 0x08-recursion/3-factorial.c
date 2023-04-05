601b003a187eae01f09d7a84d9c9372c67c745a5
rajy <kiddo.rajy@gmail.com>
2023-04-05 12:32:44 -0700

    Once an idea has taken hold of the brain it's almost impossible to eradicate

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,7 +6,7 @@
  */
 int factorial(int n)
 {
-	if (n < 0 )
+	if (n < 0)
 	{
 		return (-1);
 	}
@@ -14,5 +14,5 @@ int factorial(int n)
 	{
 		return (1);
 	}
-	return (n * factorial(n-1));
+	return (n * factorial(n - 1));
 }
